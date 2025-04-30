ef3e64581c0a84e32e0588f6a313e215d3eda550
2FKarin <101544941+2FKarin@users.noreply.github.com>
2025-04-30 15:40:01 +0700

    Update Artikel.php

--- app/Models/Artikel.php
+++ app/Models/Artikel.php
@@ -2,22 +2,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Artikel extends Model
 {
-    protected $table = 'artikel'; // Nama tabel
-    protected $fillable = ['judul', 'gambar', 'kategori', 'company', 'tanggal'];
+    protected $table = 'artikel';
     protected $primaryKey = 'id_artikel';
+    public $incrementing = true;
+    protected $keyType = 'int';
     public $timestamps = false;
 
+    protected $fillable = [
+        'id_artikel', 'judul', 'isi', 'gambar', 'company', 'kategori', 'tanggal'
+    ];
 
-    // Jika ingin format tanggal otomatis
-    //protected $dates = ['created_at', 'updated_at'];
-
-    // Relasi jika punya model Kategori (opsional, tergantung kebutuhan)
-    // public function kategoriRelasi()
-    // {
-    //     return $this->belongsTo(Kategori::class, 'kategori', 'id');
-    // }
+    public static function reorderIdArtikel()
+    {
+        DB::statement("SET @num := 0;");
+        DB::statement("UPDATE artikel SET id_artikel = (@num := @num + 1) ORDER BY id_artikel;");
+        DB::statement("ALTER TABLE artikel AUTO_INCREMENT = 1;");
+    }
 }
 
