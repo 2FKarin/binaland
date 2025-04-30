4a4f067967941e3023632e2cc3cb5a0e69bee515
2FKarin <101544941+2FKarin@users.noreply.github.com>
2025-04-30 15:41:00 +0700

    Update layout.blade.php

--- resources/views/artikel/layout.blade.php
+++ resources/views/artikel/layout.blade.php
@@ -7,9 +7,9 @@
         <div class="col-12">
             <div class="card mb-4">
                 <div class="card-header pb-0 d-flex align-items-center">
-                    <h6>Artikel</h6>
+                    <h6>Data Artikel</h6>
                     <a href="{{ route('artikel.add') }}" class="btn bg-gradient-primary btn-sm ms-auto">
-                        <span class="fa fa-plus">&nbsp;</span> Tambah
+                        <span class="fa fa-plus">&nbsp;</span> Tambah Artikel
                     </a>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
@@ -17,88 +17,111 @@
                         <table id="dataTable-artikel" class="table align-items-center mb-0">
                             <thead>
                                 <tr>
-                                    <th class="text-center">Foto</th>
-                                    <th class="text-center">Kategori</th>
-                                    <th class="text-center">Judul</th>
-                                    <th class="text-center">Publikasi</th>
-                                    <th class="text-center">Aksi</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Foto</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Perusahaan</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Kategori</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Judul</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Publikasi</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Aksi</th>
                                 </tr>
                             </thead>
-                            <tbody></tbody>
+                            <tbody>
+                                @foreach ($artikel as $row)
+                                    <tr>
+                                        <td class="text-center">
+                                        <img src="{{ asset('storage/' . $row->gambar) }}" width="100">
+                                        </td>
+                                        <td class="text-center">{{ $row->company }}</td>
+                                        <td class="lebar-tabel text-center">{{ $row->kategori }}</td>
+                                        <td class="lebar-tabel text-center">{{ $row->judul }}</td>
+                                        <td class="text-center">{{ \Carbon\Carbon::parse($row->tanggal)->format('d-m-Y') }}</td>
+                                        <td class="lebar-tabel text-center">
+                                            <a href="{{ route('artikel.edit', $row->id_artikel) }}" class="btn bg-gradient-info btn-sm">Edit
+                                            </a>
+                                            <form action="{{ route('artikel.destroy', $row->id_artikel) }}" method="POST" style="display:inline;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn bg-gradient-danger btn-sm">Hapus
+                                                </button>
+                                            </form>
+                                            <a href="{{ route('artikel.detail', $row->id_artikel) }}" class="btn bg-gradient-success btn-sm">Tinjau
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
+                        <div class="d-flex justify-content-between align-items-center px-5 mt-2">
+                            <div id="custom-info"></div>
+                            <div id="custom-pagination"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-<!-- DataTables Script -->
+<style>
+    .lebar-tabel {
+        white-space: normal !important;
+        max-width: 1000px;
+    }
+    .dataTables_wrapper .dataTables_length,
+    .dataTables_wrapper .dataTables_filter {
+        display: inline-flex;
+        align-items: center;
+        margin: 0 10px;
+    }
+    .dataTables_wrapper .dataTables_length {
+        float: left;
+        text-align: left;
+    }
+    .dataTables_wrapper .dataTables_filter {
+        float: right;
+        text-align: right;
+    }
+    .dataTables_wrapper .dataTables_paginate {
+        float: right;
+        margin-top: 1rem;
+    }
+    .dataTables_wrapper .dataTables_info {
+        margin-top: 1rem;
+        float: left;
+    }
+    .paginate_button {
+        padding: 0.375rem 0.75rem;
+        margin: 0 2px;
+        border-radius: 50%;
+        background: #f0f0f0;
+        border: none;
+        color: #333;
+        cursor: pointer;
+    }
+    .paginate_button.current {
+        background: #4e73df;
+        color: white !important;
+        border-radius: 50%;
+    }
+    .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
+        background: #e0e0e0;
+    }  
+</style>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-
-<script>
-    $(document).ready(function() {
+    <script>
+    $(document).ready(function () {
         $('#dataTable-artikel').DataTable({
-            language: {
-                url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/Indonesian.json",
-                paginate: {
-                    previous: "<",
-                    next: ">"
-                },
-            },
-            pageLength: 10,
-            ajax: {
-                url: '{{ route("artikel.data") }}',
-                type: 'POST',
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                dataSrc: ''
-            },
-            rowCallback: function(row, data) {
-                $(row).on('click', function() {
-                    window.location.href = '/artikel/' + data.id;
-                });
-            },
-            columns: [
-                {
-                    data: 'gambar',
-                    render: function(data) {
-                        var path = '/storage/artikel/' + data;
-                        return '<img src="' + path + '" class="img-fluid" style="max-width:100px;max-height:100px;">';
-                    }
-                },
-                {
-                    data: 'tanggal',
-                    render: function(data) {
-                        return '<p class="text-xs font-weight-bold mb-0">' + data + '</p>';
-                    }
-                },
-                {
-                    data: 'judul',
-                    render: function(data, type, row) {
-                        return '<p class="text-xs font-weight-bold mb-0">' + data + '</p><p class="text-xs text-secondary mb-0">' + row.company + '</p>';
-                    }
-                },
-                {
-                    data: 'tanggal',
-                    render: function(data) {
-                        return '<p class="text-xs font-weight-bold mb-0">' + data + '</p>';
-                    }
-                },
-                {
-                    data: null,
-                    render: function(data) {
-                        return `
-                            <a href="/artikel/${data.id}/edit" class="btn btn-sm bg-gradient-info">
-                                <span class="fa fa-pencil"></span> Edit
-                            </a>
-                            <a href="/artikel/${data.id}/delete" class="btn btn-sm bg-gradient-danger">
-                                <span class="fa fa-trash"></span> Hapus
-                            </a>`;
-                    }
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/Indonesian.json",
+                "paginate": {
+                    "previous": "<",
+                    "next": ">"
                 }
+            },
+            "pageLength": 10,
+            "dom": '<"top"lfr>t<"bottom"ip><"clear">',
+            "order": [
+                [4, 'desc']
             ]
         });
     });
