1f842649f8a52ec5c2610b4de646f716923c654e
2FKarin <101544941+2FKarin@users.noreply.github.com>
2025-04-30 15:39:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\ResetPassword;
 use App\Http\Controllers\ChangePassword;
-use App\Http\Controllers\ArtikelController;            
-            
+use App\Http\Controllers\ArtikelController;
+use App\Http\Controllers\LokasiController;       
 
 Route::get('/', function () {return redirect('/dashboard');})->middleware('auth');
 	Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
@@ -37,11 +37,24 @@ Route::get('/', function () {return redirect('/dashboard');})->middleware('auth'
 	Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
 	Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
 	Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
-	Route::get('/artikel', [ArtikelController::class, 'index'])->name('artikel.index');
-    Route::post('/artikel/data', [ArtikelController::class, 'data'])->name('artikel.data');
-    Route::get('/artikel/add', [ArtikelController::class, 'add'])->name('artikel.add');
-    Route::get('/artikel/{id}/edit', [ArtikelController::class, 'edit'])->name('artikel.edit');
-    Route::get('/artikel/{id}/delete', [ArtikelController::class, 'delete'])->name('artikel.delete');
+	// Menu Artikel
+	Route::get('/artikel', [ArtikelController::class, 'index'])->name('artikel.layout')->middleware('auth');
+	Route::get('/artikel/add', [ArtikelController::class, 'add'])->name('artikel.add')->middleware('auth');
+	Route::post('/artikel/store', [ArtikelController::class, 'store'])->name('artikel.store')->middleware('auth');
+	Route::get('/artikel/{id}/edit', [ArtikelController::class, 'edit'])->name('artikel.edit')->middleware('auth');
+	Route::put('/artikel/{id}', [ArtikelController::class, 'update'])->name('artikel.update')->middleware('auth');
+	Route::delete('/artikel/{id}', [ArtikelController::class, 'destroy'])->name('artikel.destroy')->middleware('auth');
+	Route::get('/artikel/detail/{id}', [ArtikelController::class, 'detail'])->name('artikel.detail')->middleware('auth');
+	//Menu Lokasi dan Gambarnya
+	Route::get('/lokasi', [LokasiController::class, 'index'])->name('lokasi.layout')->middleware('auth');
+	Route::get('/lokasi/add', [LokasiController::class, 'add'])->name('lokasi.add')->middleware('auth');
+    Route::post('/lokasi/store', [LokasiController::class, 'store'])->name('lokasi.store')->middleware('auth');
+    Route::get('/lokasi/edit/{id}', [LokasiController::class, 'edit'])->name('lokasi.edit')->middleware('auth');
+    Route::put('/lokasi/{id}', [LokasiController::class, 'update'])->name('lokasi.update')->middleware('auth');
+    Route::delete('/lokasi/destroy/{id}', [LokasiController::class, 'destroy'])->name('lokasi.destroy')->middleware('auth');
+    Route::get('/lokasi/detail/{id}', [LokasiController::class, 'detail'])->name('lokasi.detail')->middleware('auth');
+
+
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
 	Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
