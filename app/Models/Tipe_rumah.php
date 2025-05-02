f826df5b626a55a1b641032a5b327ce58ec63923
2FKarin <fajrulfalah.peta@gmail.com>
2025-05-02 07:16:47 +0700

    Update Tipe_rumah.php

---
+++ app/Models/Tipe_rumah.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tipe_rumah extends Model
+{
+    protected $table = 'tipe_rumah';
+    protected $primaryKey = 'id_tipe_lokasi';
+    protected $fillable = [
+        'id_tipe_rumah', 'id_tipe_lokasi', 'nama_tipe', 'luas_bangunan', 'luas_tanah', 'harga', 'jumlah_kamar', 'jumlah_kamar_mandi', 'fasilitas_unggunan', 'bonus', 'is_promo'
+    ];
+
+    public function lokasi()
+    {
+        return $this->belongsTo(Lokasi::class, 'fk_id_lokasi', 'id_lokasi');
+    }
+
+    public function gambarRumah()
+    {
+        return $this->hasMany(Gambar_rumah::class, 'fk_id_tipe_rumah', 'id_tipe_rumah');
+    }
+}
